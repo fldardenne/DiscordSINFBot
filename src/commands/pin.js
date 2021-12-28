045e68a70d4d76e3160f02366051051d32c9409b
obiwac <obiwac@gmail.com>
2021-12-29 00:50:45 +0100

    pin: voting system

--- src/commands/pin.js
+++ src/commands/pin.js
@@ -1,7 +1,12 @@
-const fs = require("fs");
 const { SlashCommandBuilder } = require("@discordjs/builders");
 
 const ROLE_NAME = "Pineur";
+// const VOTE_SECONDS = 5 * 60; // 5 min
+const VOTE_SECONDS = 2; // 5 min
+const VOTE_THRESHOLD = 5;
+
+const IN_FAVOUR_REACTION = "✅";
+const AGAINST_REACTION = "❌";
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -23,26 +28,62 @@ module.exports = {
 			return interaction.reply({ content: "Message could not be found! Are you sure you're passing a valid message ID?", ephemeral: true });
 		}
 
-		let permission = false;
-
-		// do we have the special role that allows us to pin?
+		// if we have the special role that allows us to pin, pin instantly
 
 		const member = interaction.member;
 
 		if (member.roles.cache.some(role => role.name === ROLE_NAME)) {
-			permission |= true;
+			await message.pin();
+			return interaction.reply({ content: "Message has been pinned!", ephemeral: true });
 		}
 
-		if (!permission) {
-			let rv =  interaction.reply({ content: "Insufficient privileges to pin this message. This is a very severe offence. This incident will be reported to the administrator.", ephemeral: true });
+		// otherwise, setup a vote
+
+		message.react(IN_FAVOUR_REACTION);
+		message.react(AGAINST_REACTION);
+
+		const filter = (reaction, _) => {
+			return reaction.emoji.name in [IN_FAVOUR_REACTION, AGAINST_REACTION];
+		};
+
+		message.awaitReactions({ filter, max: 1, time: 1000 * VOTE_SECONDS })
+
+		setTimeout(() => {
+			// refresh the cache
 			
-			fs.appendFileSync("log", `[${new Date()}] ${member.user.tag} attempted to pin a message without sufficient privileges`);
-			return rv;
-		}
+			message.react(IN_FAVOUR_REACTION);
+			message.react(AGAINST_REACTION);
 
-		// finally pin the message
+			const in_favour = message.reactions.cache.get(IN_FAVOUR_REACTION)?.count;
+			const against = message.reactions.cache.get(AGAINST_REACTION)?.count;
 
-		await message.pin();
-		return interaction.reply({ content: "Message has been pinned!", ephemeral: true });
+			console.log(in_favour, against);
+
+			if (!in_favour || !against) {
+				console.log("[pin vote] someone removed one or more of the reactions");
+				return;
+			}
+
+			// account for the bot's reaction
+
+			in_favour--;
+			against--;
+
+			if (in_favour < VOTE_THRESHOLD) {
+				console.log(`[pin vote] only ${in_favour} people voted in favour of pinning this message (threshold is ${VOTE_THRESHOLD})`);
+				return;
+			}
+
+			if (against >= in_favour) {
+				console.log(`[pin vote] more or the same number of people voted against the pin as in favour of the pin (${against} vs ${in_favour})`);
+				return;
+			}
+
+			// finally, pin the message
+
+			message.pin();
+		}, 1000 * VOTE_SECONDS);
+
+		return interaction.reply({ content: "Insufficient privileges to pin this message. We will now proceed to a vote! After 5 minutes, if there are at least 5 votes in favour of this pin and there are more people in favour than against it, your message will be pinned!", ephemeral: true });
 	}
 }
