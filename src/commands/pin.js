5be50b4f72f9d7ed3411c1cf8ff868ba70fd020f
obiwac <obiwac@gmail.com>
2021-12-28 19:48:34 +0100

    pin: checking if user has sufficient privileges before pinning

--- src/commands/pin.js
+++ src/commands/pin.js
@@ -1,4 +1,7 @@
+const fs = require("fs");
 const { SlashCommandBuilder } = require("@discordjs/builders");
+
+const ROLE_NAME = "Pineur";
 
 module.exports = {
 	data: new SlashCommandBuilder()
@@ -20,8 +23,26 @@ module.exports = {
 			return interaction.reply({ content: "Message could not be found! Are you sure you're passing a valid message ID?", ephemeral: true });
 		}
 
-		await message.pin();
+		let permission = false;
 
+		// do we have the special role that allows us to pin?
+
+		const member = interaction.member;
+
+		if (member.roles.cache.some(role => role.name === ROLE_NAME)) {
+			permission |= true;
+		}
+
+		if (!permission) {
+			let rv =  interaction.reply({ content: "Insufficient privileges to pin this message. This is a very severe offence. This incident will be reported to the administrator.", ephemeral: true });
+			
+			fs.appendFileSync("log", `[${new Date()}] ${member.user.tag} attempted to pin a message without sufficient privileges`);
+			return rv;
+		}
+
+		// finally pin the message
+
+		await message.pin();
 		return interaction.reply({ content: "Message has been pinned!", ephemeral: true });
 	}
 }
