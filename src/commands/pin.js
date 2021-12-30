835479b4255281b7cd36b4abb6a30199f92dab4d
obiwac <obiwac@gmail.com>
2021-12-30 13:28:44 +0100

    pin: rich embeds when voting

--- src/commands/pin.js
+++ src/commands/pin.js
@@ -1,16 +1,31 @@
 const { SlashCommandBuilder } = require("@discordjs/builders");
+const { MessageEmbed, MessageAttachment } = require("discord.js");
 
 const ROLE_NAME = "Pineur";
-// const VOTE_SECONDS = 5 * 60; // 5 min
-const VOTE_SECONDS = 3; // 5 min
+const VOTE_MINUTES = 5;
 const VOTE_THRESHOLD = 5;
-const VOTE_EXCLUDE_AUTHOR = false; // true
+const VOTE_EXCLUDE_AUTHOR = false;
 
 const IN_FAVOUR_REACTION = "✅";
 const AGAINST_REACTION = "❌";
 
-function log(str) {
-	console.log(str);
+const ICONS = {
+	sadge:  "https://cdn.discordapp.com/emojis/852922249098952764.png?size=96",
+	salute: "https://cdn.discordapp.com/emojis/852922249442230292.png?size=96",
+	waah:   "https://cdn.discordapp.com/emojis/852922249065660476.png?size=96",
+	angry:  "https://cdn.discordapp.com/emojis/852922248997896192.png?size=96",
+	silly:  "https://cdn.discordapp.com/emojis/852922249353756732.png?size=96",
+	uncool: "https://cdn.discordapp.com/emojis/852922249223995412.png?size=96",
+};
+
+function gen_rich(content, icon, colour) {
+	const embed = new MessageEmbed()
+		.setTitle("Pin vote")
+		.setDescription(content)
+		.setThumbnail(ICONS[icon])
+		.setColor(colour);
+
+	return embed;
 }
 
 module.exports = {
@@ -63,10 +78,10 @@ module.exports = {
 			return true;
 		};
 		
-		const collector = message.createReactionCollector({ filter, time: VOTE_SECONDS * 1000 });
+		const collector = message.createReactionCollector({ filter, time: VOTE_MINUTES * 60 * 1000 });
 		
 		collector.on('collect', (reaction, user) => {
-			log(`${user.tag} reacted with ${reaction.emoji.name}`);
+			// console.log(`${user.tag} reacted with ${reaction.emoji.name}`);
 		});
 		
 		collector.on('end', collected => {
@@ -76,33 +91,53 @@ module.exports = {
 			let against = 0;
 
 			for (let reaction of collected) {
-				log(reaction[0]);
-				
 				const name = reaction[0];
-
+				
 				in_favour += name === IN_FAVOUR_REACTION;
 				against += name === AGAINST_REACTION;
 			}
 
-			log(`[pin vote] ${in_favour} people voted in favour, ${against} people voted against`);
+			// log(`[pin vote] ${in_favour} people voted in favour, ${against} people voted against`);
 
 			// decide whether or not to pin
 
 			if (in_favour < VOTE_THRESHOLD) {
-				log(`[pin vote] only ${in_favour} people voted in favour of pinning this message (threshold is ${VOTE_THRESHOLD})`);
+				let rich;
+				
+				if (in_favour == 0) {
+					rich = gen_rich(`No one voted in favour of pinning this message (threshold is ${VOTE_THRESHOLD}). Message will not be pinned.`, "silly", "#ff7777");
+				}
+
+				else if (in_favour == 1) {
+					rich = gen_rich(`Only one person voted in favour of pinning this message (threshold is ${VOTE_THRESHOLD}). Message will not be pinned.`, "sadge", "#ff7777");
+				}
+
+				else {
+					rich = gen_rich(`Only ${in_favour} people voted in favour of pinning this message (threshold is ${VOTE_THRESHOLD}). Message will not be pinned.`, "uncool", "#ff7777");
+				}
+
+				message.reply({ embeds: [rich] });
 				return;
 			}
 
 			if (against >= in_favour) {
-				log(`[pin vote] more or the same number of people voted against the pin as in favour of the pin (${against} vs ${in_favour})`);
+				const rich = gen_rich(`More or the same number of people voted against the pin as in favour of the pin (${against} vs ${in_favour}). Message will not be pinned.`, "waah", "#ff7777");
+
+				message.reply({ embeds: [rich] });
 				return;
 			}
 
 			// finally, pin the message
 
+			const rich = gen_rich("Vote was successful! Message will now be pinned!", "salute", "#77ff77");
+			message.reply({ embeds: [rich] });
+
 			message.pin();
 		});
 
-		return interaction.reply({ content: "Insufficient privileges to pin this message. We will now proceed to a vote! After 5 minutes, if there are at least 5 votes in favour of this pin and there are more people in favour than against it, your message will be pinned!", ephemeral: true });
+		const rich = gen_rich(`${interaction.user.tag} has insufficient privileges to pin this message! We will now proceed to a vote! After ${VOTE_MINUTES} minutes, if there are at least ${VOTE_THRESHOLD} votes in favour of this pin and there are more people in favour than against it, your message will be pinned!`, "angry", "#ff7733");
+		message.reply({ embeds: [rich] });
+
+		return interaction.reply({ content: "Insufficient privileges to pin this message!", ephemeral: true });
 	}
 }
