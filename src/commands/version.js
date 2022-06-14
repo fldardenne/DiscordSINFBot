a6ae308912e236f64b3ebb1201b8ab8d0617f104
Florent Dardenne <dardenneflorent@outlook.com>
2022-06-14 16:17:56 +0200

    Revert "`/register` command (#52)"
    This reverts commit b40f728bc488b49ded1527bb0a5c4614848ffe34.

--- src/commands/version.js
+++ src/commands/version.js
@@ -1,24 +1,14 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
-const { execSync } = require('child_process')
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('version')
-    .setDescription('Return branch & commit hash the bot is running on'),
-
+    .setDescription('Return the commit hash on which the bot run'),
   async execute(client, interaction) {
-    const commit = execSync('git rev-parse HEAD')
+    revision = require('child_process')
+      .execSync('git rev-parse HEAD')
       .toString()
       .trim()
-
-    const branch = execSync('git rev-parse --abbrev-ref HEAD')
-      .toString()
-      .trim()
-
-    const remote = execSync('git config --get remote.origin.url')
-      .toString()
-      .trim()
-
-    return interaction.reply(`Running on commit \`${commit}\`, branch \`${branch}\` (\`${remote}\`)`)
+    return interaction.reply('Running on the commit ' + revision)
   },
 }
