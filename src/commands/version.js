b40f728bc488b49ded1527bb0a5c4614848ffe34
Obiwac <obiwac@gmail.com>
2022-06-14 15:52:51 +0200

    `/register` command (#52)
    * unpin: first draught
    
    * pin & unpin: indentation
    
    * unpin: make sure message is already pinned before unpinning
    
    * deploy-commands: Ignore hidden files
    
    * version: Print out a little more info
    
    * register: Started work on channel registration
    
    * register: Allow users more granular control over the course channels they register to
    
    * pin: Vote from member with pinner role instantly finished the vote
    
    * pin: Exclude pinner 2: Electric Boogaloo
    
    * register: Actually add users to channels
    
    * register: Add default `channels.json` file

--- src/commands/version.js
+++ src/commands/version.js
@@ -1,14 +1,24 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
+const { execSync } = require('child_process')
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('version')
-    .setDescription('Return the commit hash on which the bot run'),
+    .setDescription('Return branch & commit hash the bot is running on'),
+
   async execute(client, interaction) {
-    revision = require('child_process')
-      .execSync('git rev-parse HEAD')
+    const commit = execSync('git rev-parse HEAD')
       .toString()
       .trim()
-    return interaction.reply('Running on the commit ' + revision)
+
+    const branch = execSync('git rev-parse --abbrev-ref HEAD')
+      .toString()
+      .trim()
+
+    const remote = execSync('git config --get remote.origin.url')
+      .toString()
+      .trim()
+
+    return interaction.reply(`Running on commit \`${commit}\`, branch \`${branch}\` (\`${remote}\`)`)
   },
 }
