362429d7573c82ef6e9d4fcf302f6809ec40c5a1
obiwac <obiwac@gmail.com>
2022-09-03 20:44:00 +0200

    crash: Command for testing shit

---
+++ src/commands/crash.js
@@ -0,0 +1,10 @@
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName('crash')
+		.setDescription('Crashes the bot. Used for debugging purposes.'),
+
+	async execute(client, interaction) {
+		throw "Crash!"
+		return interaction.reply("The bot should have crashed!")
+	},
+}
