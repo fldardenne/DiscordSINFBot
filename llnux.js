583158cb0a6ccef989e661005fb7be90a2b7f51b
Obiwac <obiwac@gmail.com>
2022-11-23 01:42:28 +0100

    LITTLE SURPRISE BROOOOO

---
+++ llnux.js
@@ -0,0 +1,11 @@
+// Once upon a time in our little galaxy far away, a little kap had access to the private account of Obiwac
+// Some would it's a bad idea, other would say it's the opportunity to shine.
+// I would say it's the opportunity to deploy the awesome opportunity to present the awesome person
+// able to keep the world moving.
+// While it's a difficult place to live, it's still the perfect location to understand the human being that 
+// wandering inside what we call, our world.
+// Please enjoy and try to understand every human being you meet. They are all complex and moved by some
+// difficult motivation.
+// Bisous ♥  🥒(i'm a pickle btw, best shit i've ever seen)
+
+
