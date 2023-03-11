4d9a03052070e55d5efa1d71d75dd2ef6eb687e3
RappyTV <rappytv@rappytv.com>
2023-03-11 21:14:13 +0100

    index.js cleanup

--- src/bot.js
+++ src/bot.js
@@ -1,8 +1,8 @@
 // Require the necessary discord.js classes
-const fs = require('fs')
-const { Client, Collection, Intents } = require('discord.js')
-const dotenv = require('dotenv')
-dotenv.config()
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const client = new Client({
 	intents: [
@@ -12,57 +12,37 @@ const client = new Client({
 	]
 });
 
-// register the set of commands dynamically by reading the ./commands folder
-client.commands = new Collection()
-const commandFiles = fs
-	.readdirSync('./commands')
-	.filter(file => file.endsWith('.js'))
-
-for (const file of commandFiles) {
-	const command = require(`./commands/${file}`)
-	client.commands.set(command.data.name, command)
-}
-
 // if there is an event and this event is a command
 client.on('interactionCreate', async interaction => {
-	if (!interaction.isCommand()) return
+	if(!interaction.isCommand()) return;
 
-	const command = client.commands.get(interaction.commandName)
-
-	if (!command) {
-		return
-	}
+	const command = client.commands.get(interaction.commandName);
+	if(!command) return interaction.reply({ content: "Unknown command!", ephemeral: true });
 
 	try {
-		console.log(command)
-		await command.execute(client, interaction)
+		console.log(command);
+		await command.execute(client, interaction);
+	} catch(err) {
+		console.error(err);
+		return interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
 	}
+});
 
-	catch (error) {
-		console.error(error)
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
 
-		return interaction.reply({
-			content: 'There was an error while executing this command!',
-			ephemeral: true,
-		})
-	}
-})
+// register the set of commands dynamically by reading the ./commands folder
+for(const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	client.commands.set(command.data.name, command);
+}
+// register the set of events dynamically by reading the ./events folder
+for(const file of eventFiles) {
+	const event = require(`./events/${file}`);
 
-// other event
-const eventFiles = fs
-	.readdirSync('./events')
-	.filter(file => file.endsWith('.js'))
-
-for (const file of eventFiles) {
-	const event = require(`./events/${file}`)
-
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(client, ...args))
-	}
-
-	else {
-		client.on(event.name, (...args) => event.execute(client, ...args))
-	}
+	if(event.once) client.once(event.name, (...args) => event.execute(client, ...args));
+	else client.on(event.name, (...args) => event.execute(client, ...args));
 }
 
-client.login(process.env.DISCORD_TOKEN)
+client.login(process.env.DISCORD_TOKEN);
